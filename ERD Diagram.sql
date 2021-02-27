a42f59647b06a633ad08c90834dcc20710b524bc
code_red <72528267+Corters22@users.noreply.github.com>
2021-02-26 22:29:55 -0600

    added sql queries and erd diagram

---
+++ ERD Diagram.sql
@@ -0,0 +1,48 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/UJqbeA
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Employee_Titles" (
+    "Title_id" string   NOT NULL,
+    "Title" string   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "employee_no" int   NOT NULL,
+    "Title_id" string   NOT NULL,
+    "Birthdate" datetime   NOT NULL,
+    "First_Name" string   NOT NULL,
+    "Last_Name" string   NOT NULL,
+    "Sex" string   NOT NULL,
+    "Hire_date" datetime   NOT NULL
+);
+
+CREATE TABLE "Departments" (
+    "Dept_number" string   NOT NULL,
+    "Dept_name" string   NOT NULL
+);
+
+-- Table documentation comment 1 (try the PDF/RTF export)
+-- Table documentation comment 2
+CREATE TABLE "Salary" (
+    "employee_no" int   NOT NULL,
+    "Salary" money   NOT NULL
+);
+
+CREATE TABLE "Dept_Manager" (
+    "Dept_number" string   NOT NULL,
+    "employee_no" int   NOT NULL
+);
+
+CREATE TABLE "Dept_Employee" (
+    "employee_no" int   NOT NULL,
+    "Dept_number" string   NOT NULL
+);
+
+CREATE INDEX "idx_Employee_Titles_Title_id"
+ON "Employee_Titles" ("Title_id");
+
---
+++ EmployeeSQL/homework.sql
@@ -0,0 +1,41 @@
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees as e
+join salaries as s on
+e.emp_no=s.emp_no
+;
+
+select e.first_name, e.last_name, e.hire_date
+from employees as e
+where e.hire_date between '1986-01-01' and '1986-12-31'
+order by e.hire_date asc
+;
+
+select dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+from dept_manager as dm
+join departments as d 
+	on d.dept_no=dm.dept_no
+join employees as e 
+	on dm.emp_no=e.emp_no
+;
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+join dept_emp as de
+	on de.emp_no=e.emp_no
+join departments as d
+	on d.dept_no=de.dept_no
+;
+
+select e.first_name, e.last_name, e.sex
+from employees as e
+where e.first_name = 'Hercules' and e.last_name like 'B%'
+;
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+join dept_emp as de
+	on e.emp_no=de.emp_no
+join departments as d
+	on de.dept_no=d.dept_no
+where d.dept_name = 'Sales'
+;
